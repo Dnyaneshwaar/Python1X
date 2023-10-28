42e589a7df6200d8c25dc1227b60ba3e4c82f866
Dnyaneshwaar <dnyaneshwaar.chandwade@gmail.com>
2023-10-28 23:00:22 +0530

    Uploaded first 2 fies

---
+++ Lab002.py
@@ -0,0 +1,8 @@
+My_String1= input("First name\n")
+My_String2= input("Last Name\n")
+
+print(My_String1,My_String2)
+
+print("Length of first name\n",len(My_String1))
+print(My_String1[0:2])
+print("Dnyaneshwar" [0:6])
